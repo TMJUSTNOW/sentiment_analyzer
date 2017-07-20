afb7436a9b2c695050d18187e2fddfa8ff8ea570
janmejaya-nanda <john.janmejaya@gmail.com>
2017-07-20 21:34:35 +0530

    Adding Few stop words and Lemmatization

--- preprocessing_word_level.py
+++ preprocessing_word_level.py
@@ -6,8 +6,11 @@ import re
 import pickle
 import time
 
+import nltk
 from nltk.corpus import stopwords
-from nltk.tokenize import word_tokenize
+from nltk.tokenize import word_tokenize, sent_tokenize
+from nltk.stem.wordnet import WordNetLemmatizer
+from nltk.corpus import wordnet
 
 # Read data set(IMDB Review data)
 train_pos_sample_dir = '/home/janmejaya/sentiment_analyzer/aclImdb/train/pos'
@@ -27,6 +30,21 @@ vocab_to_idx = {}
 vocab_frequency = {}
 len_vocab = 0
 index_to_start = 1      # 0th index is used for padding
+
+def get_wordnet_pos(treebank_tag):
+
+    if treebank_tag.startswith('J'):
+        return wordnet.ADJ
+    elif treebank_tag.startswith('V'):
+        return wordnet.VERB
+    elif treebank_tag.startswith('N'):
+        return wordnet.NOUN
+    elif treebank_tag.startswith('R'):
+        return wordnet.ADV
+    else:
+        return ''
+
+word_lemmatizer = WordNetLemmatizer()
 
 for each_file in file_list:
     try:
@@ -53,37 +71,45 @@ for each_file in file_list:
     # Keep important punctuation
     html_cleaned_data = re.sub('[^A-Za-z ]+', '', html_cleaned_data)
 
-    # TODO: Perform Word Lemmatization on text
 
     # Break data into words and remove stop Words(or unnecessary words which doesn't have much influence on meaning)
     # TODO: There are few words like 'br', 'o', 'or' after listed to remove still appears in sentence
-    stop_words = ['after', 'others', 'clud', 'would', 'id', 'youll', 'men', 'be', 'yourselves', 'im', 'with', 'only', 'is', 'hadn', 'll', 'into', 'being', 'a', 'me', 'has', 'been', 'why', 'where', 'up', 'can', 'than', 'in', 'from', 'who', 'the', 'should', 'your', 'themselves', 'while', 'we', 'against', 'further', 'these', 'those', 'were', 'be', 'out', 'him', 'our', 'which', 'just', 'doing', 'this', 'had', 'theirs', 'i', 'under', 'm', 'herself', 'by', 'at', 'until', 'here', 'were', 'if', 'myself', 'ma', 'ours', 'as', 'all', 'each', 'how', 'when', 'other', 'itself', 'an', 'my', 'did', 'them', 'ourselves', 'during', 'whom', 'am', 'o', 'same', 'are', 'have', 'above', 'what', 'both', 'do', 'off', 'before', 'or', 'having', 'now', 'too', 'on', 'and', 'through', 'she', 'his', 'do', 'there', 'won', 'they', 'to', 'are', 't', 'few', 'was', 'it', 'did', 'himself', 'her', 'some', 'such', 'have', 'yours', 'more', 'for', 're', 'will', 'ain', 's', 'you', 'their', 'about', 'between', 'that', 'once', 'does', 'shall', 'no', 've', 'he', 'of', 'y', 'then', 'own', 'again', 'd', 'any', 'not', 'does', 'was', 'its', 'below', 'hers', 'most', 'but', 'so', 'down', 'yourself']
-    movie_stop_words = ['actors', 'story', 'villain', 'chemistry', 'bill', 'lee', 'audiences', 'producers', 'filmed', 'review', 'song', 'musical', 'songs', 'thriller', 'theater', 'br', 'dialog', 'james', 'one', 'first', 'four', 'five', 'ten', 'gore', 'ru', 'filmbr', 'viewers', 'dr', 'era','stefan', 'jrs', 'mar', 'palestinian', 'gandolfini', 'elisha', 'taboos', 'jessie', 'hindu', 'gandolfini', 'gandolfini', 'water', 'de', 'mov', 'moviebr', 'robert', 'v', 'liv', 'david', 'w', 'la', 'sitt', 'volv', 'tak', 'michael', 'ope', 'mr', 'hour', 'movie', 'music', 'film', 'two', 'th', 'youre', 'john', 'bor', 'ett', 'hollywood', 'drama', 'theyre', 'yeah', 'com', 'itbr', 'genre']
-    tokenized_word = word_tokenize(html_cleaned_data)
-    filtered_sentences = [w.lower() for w in tokenized_word if w.lower() not in stop_words+movie_stop_words]
+    stop_words = ['after', 'others', 'clud', 'would', 'id', 'time', 'youll','brother', 'second', 'shes', 'becomes', 'youd','etc', 'college','setting', 'ok', 'hair','wife', 'eye','thats','come', 'plot', 'english', 'so', 'house','year','men', 'be', 'go', 'line', 'lines','three', 'couple', 'next', 'home','else', 'saw','work', 'may', 'hes', 'day', 'goes', 'guy','theres','minutes', 'done', 'world', 'could', 'yourselves', 'us', 'actually','years', 'man', 'im', 'with', 'only', 'is', 'had', 'll', 'into', 'being', 'a', 'me', 'has', 'been', 'why', 'where', 'up', 'can', 'than', 'in', 'sex', 'days','from', 'who', 'the', 'should', 'your', 'themselves', 'while', 'we', 'against', 'further', 'these', 'those', 'were', 'be', 'out', 'him', 'our', 'which', 'just', 'doing', 'this', 'had', 'theirs', 'i', 'under', 'm', 'herself', 'by', 'at', 'until', 'here', 'were', 'if', 'myself', 'ma', 'ours', 'as', 'all', 'each', 'how', 'when', 'other', 'itself', 'an', 'my', 'did', 'them', 'ourselves', 'during', 'whom', 'am', 'o', 'same', 'are', 'have', 'above', 'what', 'both', 'do', 'off', 'before', 'or', 'having', 'now', 'too', 'on', 'and', 'through', 'she', 'his', 'do', 'there', 'won', 'they', 'to', 'are', 't', 'few', 'was', 'it', 'did', 'himself', 'her', 'such', 'have', 'yours', 'more', 'for', 're', 'will', 'ain', 's', 'you', 'their', 'about', 'between', 'that', 'once', 'does', 'shall', 've', 'he', 'of', 'y', 'own', 'again', 'd', 'any', 'does', 'was', 'its', 'below', 'hers', 'yourself']
+    movie_stop_words = ['actors', 'actor', 'dvd',  'story', 'villain','acting', 'u', 'family', 'oh', 'cinematic','dialogue', 'joe', 'sexy','japanese','think', 'cinematography', 'father', 'say','studio', 'boy', 'direction', 'cinema', 'friend', 'writ','movies', 'american', 'actress','classic','star', 'directed', 'bor', 'title', 'human','enterta', 'ett', 'production', 'performances','films','characters', 'character', 'tv', 'main', 'sense','woman','girl', 'scenes', 'terest', 'scene', 'mak', 'director', 'ive', 'back', 'th','chemistry', 'bill', 'lee', 'audiences', 'producers', 'filmed', 'review', 'song', 'musical', 'songs', 'thriller', 'theater', 'br', 'dialog', 'james', 'one', 'first', 'four', 'five', 'ten', 'gore', 'ru', 'filmbr', 'viewers', 'dr', 'era','stefan', 'jrs', 'mar', 'palestinian', 'gandolfini', 'elisha', 'taboos', 'jessie', 'hindu', 'gandolfini', 'gandolfini', 'water', 'de', 'mov', 'moviebr', 'robert', 'v', 'liv', 'david', 'w', 'la', 'sitt', 'volv', 'tak', 'michael', 'ope', 'mr', 'hour', 'movie', 'music', 'film', 'two', 'th', 'youre', 'john', 'bor', 'ett', 'hollywood', 'drama', 'theyre', 'yeah', 'com', 'itbr', 'genre']
+
+    # Performing Word Lemmatization on text
+    lemmatized_data = []
+    for word, typ in nltk.pos_tag(word_tokenize(html_cleaned_data)):
+        typ = get_wordnet_pos(typ)
+        if typ:
+            lemmatized_data.append(word_lemmatizer.lemmatize(word, typ))
+        else:
+            lemmatized_data.append(word_lemmatizer.lemmatize(word))
+
+    filtered_sentences = [w.strip(' ').lower() for w in lemmatized_data if w.strip(' ').lower() not in stop_words+movie_stop_words]
+    if 'be' in filtered_sentences or 'o' in filtered_sentences or 'or' in filtered_sentences or 'br' in filtered_sentences:
+        print(lemmatized_data)
+        print(filtered_sentences)
+        print('\n\n')
+        time.sleep(10)
 
 
-    # Convert data into standard lexicons
-    # Remove 'ing'
-    # TODO: Look of any other library doing it or try to add few other to list
-    removing_string = ['ing']
-    for val in removing_string:
-        filtered_sentences = [w.strip(val) if w.endswith(val) else w for w in filtered_sentences]
 
     # TODO: Add low frequency words from classes
     # create word to index and index to word dictionary
     vocab = set(filtered_sentences)
     for indx, val in enumerate(vocab):
-        if val not in vocab_to_idx:
-            vocab_to_idx[val] = len_vocab + index_to_start
-            idx_to_vocab[len_vocab + index_to_start] = val
-            len_vocab += 1
+        # if val not in vocab_to_idx:
+        #     vocab_to_idx[val] = len_vocab + index_to_start
+        #     idx_to_vocab[len_vocab + index_to_start] = val
+        #     len_vocab += 1
 
         # Count frequency of the Word
         if val in vocab_frequency:
             vocab_frequency[val] += 1
         else:
             vocab_frequency[val] = 1
+            len_vocab += 1
     # TODO: Check most frequent occurring words in
 
 # Add Extra Padding words
@@ -92,9 +118,15 @@ idx_to_vocab[0] = '<pad>'
 # Save data to pickle object
 print("Length of Vocab", len_vocab)
 
-frequent_words_tuple = sorted(vocab_frequency.items(), key=lambda x: x[1], reverse=True)[:1000]
-frequent_words = [val for val, freq in frequent_words_tuple]
+frequent_words_tuple = sorted(vocab_frequency.items(), key=lambda x: x[1], reverse=True)
+frequent_words = [val for val, freq in frequent_words_tuple][:1000]
 print(frequent_words)
+# Before saving Dictionary create index of vocab based on descending order of their frequency
+idx = 1
+for val, freq in frequent_words_tuple:
+    vocab_to_idx[val] = idx
+    idx_to_vocab[idx] = val
+    idx += 1
 print('Saving Dictionary')
 dictionary = {'vocab_to_index': vocab_to_idx, 'index_to_vocab': idx_to_vocab, 'vocab_frequency': vocab_frequency}
 with open('data/movie_vocab.pkl', 'wb') as f:
@@ -115,7 +147,7 @@ vocab_len = len(data['vocab_to_index'])
 print('Vocab len: ', vocab_len)
 
 max_word = 50
-max_features = 20000
+max_features = 20001
 file_pointer = 0
 def next_batch(batch_size, test=False):
     # This function reads equal amount of positive and negative review depending on batch size.
@@ -190,13 +222,13 @@ def next_batch(batch_size, test=False):
         input_data[index] = truncated_review
     return (input_data, target_data)
 
-data_x, data_y = next_batch(25000)
+data_x, data_y = next_batch(len(file_list))
 train_dict = {'input': data_x, 'target': data_y}
 
 with open('data/train.pkl', 'wb') as f:
     pickle.dump(train_dict, f)
 
-data_x, data_y = next_batch(25000, test=True)
+data_x, data_y = next_batch(len(file_list), test=True)
 test_dict = {'input': data_x, 'target': data_y}
 
 with open('data/test.pkl', 'wb') as f:
